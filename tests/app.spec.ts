b31d6a27f1f9199cab50b9ba2c3684deb58e6711
plondon <philip.n.london@gmail.com>
2023-03-23 15:54:57 -0400

    test(app): bitcoin derivation

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,5 +1,26 @@
+import {getAddress} from '../src/app';
+
+jest.mock('../src/router', () => ({
+  getNextReceiveIndex: jest.fn().mockResolvedValue({
+    bip44Path: 0,
+    index: 0,
+    isRegistered: false,
+  }),
+}));
+
 describe('app', () => {
-  it('runs', () => {
-    expect(true).toBe(true);
+  describe('getAddress', () => {
+    it('runs', async () => {
+      const address = await getAddress({
+        user: 'asdf',
+        assetId: 'bitcoin',
+        custody: 'internal',
+        wallet: 0,
+      });
+
+      expect(address).toStrictEqual({
+        address: 'bc1qyvlz4t2y9c9ksfd7uu9kfv8rmhhjxvfwrnyqmc',
+      });
+    });
   });
 });
