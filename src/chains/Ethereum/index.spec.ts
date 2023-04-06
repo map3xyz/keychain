c5ef2d29bd97176c41fcd07dbd235935a86b9180
plondon <philip.n.london@gmail.com>
2023-04-06 16:14:04 -0700

    test(send): test build tx result

--- src/chains/Ethereum/index.spec.ts
+++ src/chains/Ethereum/index.spec.ts
@@ -40,4 +40,29 @@ describe('chains', () => {
       );
     });
   });
+
+  describe('send', () => {
+    it('ethereum', async () => {
+      jest.spyOn(storeApi, 'getNextReceiveIndex').mockResolvedValueOnce({
+        addressIndex: 0,
+        bip44Path: 60,
+        isRegistered: false,
+      });
+      jest.spyOn(storeApi, 'registerAddress').mockResolvedValueOnce({
+        status: 'ok',
+      });
+      const txid = await keychain.send({
+        amount: '1',
+        assetId: 'ethereum',
+        memo: 'test',
+        to: '0xA3Dcd899C0f3832DFDFed9479a9d828c6A4EB2A7',
+        userId: 'asdf',
+        walletId: 0,
+      });
+
+      expect(txid).toBe(
+        '0xf86a018506fc23ac0082520894a3dcd899c0f3832dfdfed9479a9d828c6a4eb2a7863362ee6e0800802ea0e300c8c94eccd46a8f894bab135fc718efbc07470043368011309b4f9364dcaea057c585ecc0574a85c1952d43cce1e23cbc25d143f0b6789481250f27e63e9c69'
+      );
+    });
+  });
 });
